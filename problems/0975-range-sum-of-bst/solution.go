8b85bab8cef7c206b96c56138582bc75e928c0ee
praveshdev3 <122818044+praveshdev3@users.noreply.github.com>
2024-09-02 16:55:38 +0000

    [LC Sync] - Range Sum of BST - Runtime - 67 ms (85.86%), Memory - 7.6 MB (91.98%)

---
+++ problems/0975-range-sum-of-bst/solution.go
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func rangeSumBST(root *TreeNode, low int, high int) int {
+    if root == nil {
+        return 0
+    }
+
+    if root.Val < low {
+        return rangeSumBST(root.Right,low,high)
+    }else if root.Val > high {
+        return rangeSumBST(root.Left,low,high)
+    }else {
+        return root.Val + rangeSumBST(root.Left,low,high) + rangeSumBST(root.Right,low,high)
+    }
+
+}

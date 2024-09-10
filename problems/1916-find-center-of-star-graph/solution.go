fc59c926be0c5dbfc168b7b2362485854752d7aa
praveshdev3 <122818044+praveshdev3@users.noreply.github.com>
2024-09-10 12:13:27 +0000

    [LC Sync] - Find Center of Star Graph - Runtime - 80 ms (94.74%), Memory - 17.6 MB (35.67%)

---
+++ problems/1916-find-center-of-star-graph/solution.go
@@ -0,0 +1,7 @@
+func findCenter(edges [][]int) int {
+    if edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1]{
+        return edges[0][0]
+    } else {
+        return edges[0][1]
+    }
+}

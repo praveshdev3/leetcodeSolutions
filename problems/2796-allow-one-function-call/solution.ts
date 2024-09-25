e24af9db2973ec96c227d8afbf7e21e0cf11ae12
praveshdev3 <122818044+praveshdev3@users.noreply.github.com>
2024-09-25 12:50:14 +0000

    [LC Sync] - Allow One Function Call - Runtime - 54 ms (73.91%), Memory - 51.6 MB (5.89%)

---
+++ problems/2796-allow-one-function-call/solution.ts
@@ -0,0 +1,24 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type OnceFn = (...args: JSONValue[]) => JSONValue | undefined
+
+function once(fn: Function): OnceFn {
+    let hasBeenCalled = false;
+    let result;
+    return function (...args) {
+        if (!hasBeenCalled){
+            result = fn(...args);
+            hasBeenCalled = true;
+            return result;
+        }else {
+            return undefined;
+        }
+    };
+}
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */

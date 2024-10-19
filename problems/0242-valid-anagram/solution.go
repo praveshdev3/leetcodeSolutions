0faa1f3863a47b2603ab3e9221ba7b9ec5907b8e
praveshdev3 <122818044+praveshdev3@users.noreply.github.com>
2024-10-19 07:05:50 +0000

    [LC Sync] - Valid Anagram - Runtime - 4 ms (59.13%), Memory - 4.6 MB (99.22%)

---
+++ problems/0242-valid-anagram/solution.go
@@ -0,0 +1,20 @@
+func isAnagram(s string, t string) bool {
+    if len(s) != len(t) {
+        return false
+    }
+
+    runeMap := map[rune]int{}
+    for _,r := range s{
+        runeMap[r]++
+    }
+
+    for _,r := range t{
+        if v,ok := runeMap[r]; ok && v>0{
+            runeMap[r]--
+        }else{
+            return false
+        }
+    }
+
+    return true
+}

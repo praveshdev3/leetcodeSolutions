9d1d2ba903aaa95e3908a5df468f044293b49ae8
praveshdev3 <122818044+praveshdev3@users.noreply.github.com>
2024-11-11 03:40:00 +0000

    [LC Sync] - Remove Element - Runtime - 0 ms (100.00%), Memory - 4.1 MB (69.01%)

---
+++ problems/0027-remove-element/solution.go
@@ -0,0 +1,10 @@
+func removeElement(nums []int, val int) int {
+    i:=0
+    for _,num := range nums{
+        if num != val{
+            nums[i] = num
+            i++
+        }
+    }
+    return i
+}

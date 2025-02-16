c98cff79deae2d2deebe2ed353acb3719d53ee18
praveshdev3 <122818044+praveshdev3@users.noreply.github.com>
2025-02-16 12:20:14 +0000

    [LC Sync] - Count Complete Tree Nodes - Runtime - 0 ms (100.00%), Memory - 9.8 MB (12.23%)

--- problems/0222-count-complete-tree-nodes/solution.go
+++ problems/0222-count-complete-tree-nodes/solution.go
@@ -7,8 +7,29 @@
  * }
  */
 func countNodes(root *TreeNode) int {
-    if root != nil {
-        return 1+countNodes(root.Left)+countNodes(root.Right)
+    leftLeg := leftLegLen(root)
+    rightLeg := rightLegLen(root)
+    if leftLeg == rightLeg{
+        return int(math.Pow(2.0,float64(leftLeg)) - 1)
+    } else {
+        return 1 + countNodes(root.Left) + countNodes(root.Right)
     }
-    return 0
 }
+
+func leftLegLen(root *TreeNode) int{
+    if root == nil{
+        return 0
+    }
+    return 1 + leftLegLen(root.Left)
+}
+
+func rightLegLen(root *TreeNode) int{
+    if root == nil{
+        return 0
+    }
+    return 1 + rightLegLen(root.Right)
+}
+    // if root != nil {
+    //     return 1+countNodes(root.Left)+countNodes(root.Right)
+    // }
+    // return 0

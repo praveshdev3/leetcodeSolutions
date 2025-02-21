b4ef2aa86f8809a8dd6e728ca2833213a01def6d
praveshdev3 <122818044+praveshdev3@users.noreply.github.com>
2025-02-21 16:15:42 +0000

    [LC Sync] - Gas Station - Runtime - 0 ms (100.00%), Memory - 10.7 MB (51.78%)

--- problems/0134-gas-station/solution.go
+++ problems/0134-gas-station/solution.go
@@ -1,17 +1,17 @@
 func canCompleteCircuit(gas []int, cost []int) int {
-    n := len(gas)
     globalFuelLeft, fuelLeft, start := 0,0,0
-    for i:=0;i<n;i++{
-        globalFuelLeft += gas[i] - cost[i]
-        fuelLeft += gas[i] - cost[i]
+    for index := range gas{
+        globalFuelLeft += gas[index] - cost[index]
+        fuelLeft += gas[index] - cost[index]
         if fuelLeft < 0{
             fuelLeft = 0
-            start = i + 1
-        }
+            start = index + 1
+        } 
     }
 
     if globalFuelLeft < 0{
         return -1
     }
+
     return start
 }

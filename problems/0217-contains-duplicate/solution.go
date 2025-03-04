2483c6e7790b4cedb8af73b997d2604d563476b1
praveshdev3 <122818044+praveshdev3@users.noreply.github.com>
2025-03-04 15:34:53 +0000

    [LC Sync] - Contains Duplicate - Runtime - 14 ms (72.36%), Memory - 13.8 MB (15.29%)

---
+++ problems/0217-contains-duplicate/solution.go
@@ -0,0 +1,12 @@
+func containsDuplicate(nums []int) bool {
+    mp := make(map[int]int)
+    for _,num:=range nums{
+        _,ok := mp[num]
+        if !ok{
+            mp[num] = 1
+        }else{
+            return true
+        }
+    }
+    return false
+}

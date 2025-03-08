071e6326a90cfcadbb0b25a889330dd14629f121
praveshdev3 <122818044+praveshdev3@users.noreply.github.com>
2025-03-08 15:58:25 +0000

    [LC Sync] - Two Sum - Runtime - 1 ms (56.86%), Memory - 6.1 MB (19.04%)

--- problems/0001-two-sum/solution.go
+++ problems/0001-two-sum/solution.go
@@ -1,12 +1,12 @@
 func twoSum(nums []int, target int) []int {
-hashmap := make(map[int]int)
-for i,num := range nums {
-    complement := target - num
-    _,ok := hashmap[complement]
-    if ok {
-        return []int{hashmap[complement],i}
+    hashmap := make(map[int]int)
+    for i,num := range nums {
+        complement := target - num
+        _,ok := hashmap[complement]
+        if ok {
+            return []int{hashmap[complement],i}
+        }
+        hashmap[num] = i
     }
-    hashmap[num] = i
-}
-return nil
+    return []int{}
 }

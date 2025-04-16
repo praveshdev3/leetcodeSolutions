cb6546596567863b6622599884f9e31a363a601b
praveshdev3 <122818044+praveshdev3@users.noreply.github.com>
2025-04-16 16:17:59 +0000

    [LC Sync] - Valid Parentheses - Runtime - 0 ms (100.00%), Memory - 4.3 MB (35.79%)

--- problems/0020-valid-parentheses/solution.go
+++ problems/0020-valid-parentheses/solution.go
@@ -2,34 +2,32 @@ func isValid(s string) bool {
     if len(s)%2 != 0{
         return false
     }
-
     stack := make([]rune,0)
-
-    for _,c := range s{
-        if c == '{' || c == '(' || c == '['{
-            stack = append(stack,c)
-        } else if len(stack) > 0{
-            switch c{
-                case ')':
-                    if stack[len(stack)-1] != '('{
-                        return false
-                    } else{
-                        stack = stack[:len(stack)-1]
-                    }
+    for _,r := range s{
+        if r == '{' || r == '[' || r == '('{
+            stack = append(stack,r)
+        }else if len(stack)>0{
+            switch r {
                 case '}':
                     if stack[len(stack)-1] != '{'{
                         return false
-                    } else{
+                    }else{
                         stack = stack[:len(stack)-1]
                     }
                 case ']':
                     if stack[len(stack)-1] != '['{
                         return false
-                    } else{
+                    }else{
+                        stack = stack[:len(stack)-1]
+                    }
+                case ')':
+                    if stack[len(stack)-1] != '('{
+                        return false
+                    }else{
                         stack = stack[:len(stack)-1]
                     }
             }
-        } else {
+        }else{
             return false
         }
     }

8949f3f8138d482ef34c615494dad9a6b16ad062
praveshdev3 <122818044+praveshdev3@users.noreply.github.com>
2025-04-21 15:43:24 +0000

    [LC Sync] - Best Time to Buy and Sell Stock - Runtime - 0 ms (100.00%), Memory - 19.2 MB (5.02%)

--- problems/0121-best-time-to-buy-and-sell-stock/solution.go
+++ problems/0121-best-time-to-buy-and-sell-stock/solution.go
@@ -1,14 +1,13 @@
 func maxProfit(prices []int) int {
     maxProfit := 0
     buyPrice := prices[0]
-    for i:=1; i<len(prices);i++{
+    for i:=1; i<len(prices); i++{
         if prices[i] < buyPrice{
             buyPrice = prices[i]
-        } else{
-            profit := prices[i] - buyPrice
-            if profit > maxProfit{
-                maxProfit = profit
-            }
+        }
+        profit := prices[i] - buyPrice
+        if maxProfit < profit {
+            maxProfit = profit
         }
     }
     return maxProfit

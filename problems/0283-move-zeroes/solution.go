a74113472533431b270d0e9a0fa53b9341bde5fd
praveshdev3 <122818044+praveshdev3@users.noreply.github.com>
2025-05-11 08:35:43 +0000

    [LC Sync] - Move Zeroes - Runtime - 0 ms (100.00%), Memory - 8.7 MB (59.68%)

--- problems/0283-move-zeroes/solution.go
+++ problems/0283-move-zeroes/solution.go
@@ -1,13 +1,24 @@
 func moveZeroes(nums []int)  {
-    if len(nums) > 1{
-        j:=0
+    if len(nums) > 1 {
+        j := 0
         for i:=1; i<len(nums); i++{
-            if nums[j] == 0 && nums[i]!=0{
-                nums[j], nums[i] = nums[i], nums[j]
+            if nums[j]==0 && nums[i] !=0{
+                nums[i],nums[j] = nums[j],nums[i]
                 j++
-            } else if nums[j] != 0{
+            }else if nums[j] != 0{
                 j++
             }
         }
     }
+    // if len(nums) > 1{
+    //     j:=0
+    //     for i:=1; i<len(nums); i++{
+    //         if nums[j] == 0 && nums[i]!=0{
+    //             nums[j], nums[i] = nums[i], nums[j]
+    //             j++
+    //         } else if nums[j] != 0{
+    //             j++
+    //         }
+    //     }
+    // }
 }

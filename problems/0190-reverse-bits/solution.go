cb2ed37999acbc30d353b65ad97c96769bcbe1a3
praveshdev3 <122818044+praveshdev3@users.noreply.github.com>
2025-05-13 15:57:01 +0000

    [LC Sync] - Reverse Bits - Runtime - 0 ms (100.00%), Memory - 4.5 MB (8.14%)

---
+++ problems/0190-reverse-bits/solution.go
@@ -0,0 +1,9 @@
+func reverseBits(num uint32) uint32 {
+    var res uint32 = 0
+    for i:=0; i<32; i++{
+        res = res << 1
+        res += (num & 1)
+        num = num >> 1
+    }
+    return res
+}

51fc4048f1f988d3413b4a0b46eb86873f5010f3
praveshdev3 <122818044+praveshdev3@users.noreply.github.com>
2025-05-18 05:56:39 +0000

    [LC Sync] - Longest Substring Without Repeating Characters - Runtime - 8 ms (37.58%), Memory - 5 MB (44.99%)

--- problems/0003-longest-substring-without-repeating-characters/solution.go
+++ problems/0003-longest-substring-without-repeating-characters/solution.go
@@ -1,14 +1,21 @@
 func lengthOfLongestSubstring(s string) int {
     mx := 0
-    mp := make(map[byte]int)
     l := 0
+    mp := make(map[byte]int)
     for r:=0; r<len(s); r++{
         mp[s[r]]++
-        for mp[s[r]] > 1 {
+        for mp[s[r]] > 1{
             mp[s[l]]--
             l++
         }
-        mx = max(mx,r-l+1)
+        mx = max(mx, r-l+1)
     }
     return mx
+}
+
+func max(a,b int) int{
+    if a>b{
+        return a
+    }
+    return b
 }

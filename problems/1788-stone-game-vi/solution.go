5757f3b265db9ff36794222bf1efb6ba3fb0215a
praveshdev3 <122818044+praveshdev3@users.noreply.github.com>
2025-06-08 13:40:58 +0000

    [LC Sync] - Stone Game VI - Runtime - 36 ms (76.92%), Memory - 10 MB (100.00%)

--- problems/1788-stone-game-vi/solution.go
+++ problems/1788-stone-game-vi/solution.go
@@ -1,27 +1,27 @@
 func stoneGameVI(aliceValues []int, bobValues []int) int {
     indexes := make([]int, len(aliceValues))
+
     for i := range aliceValues{
         indexes[i] = i
     }
 
-    sort.Slice(indexes, func(i,j int) bool {
+    sort.Slice(indexes, func(i,j int) bool{
         return aliceValues[indexes[i]] + bobValues[indexes[i]] > aliceValues[indexes[j]] + bobValues[indexes[j]]
     })
 
     res := 0
     for i,v := range indexes{
         if i%2 == 0{
-            res += aliceValues[v]
-        } else {
-            res -= bobValues[v]
+            res+=aliceValues[v]
+        }else{
+            res-=bobValues[v]
         }
     }
 
-    if res == 0{
-        return 0
-    }
-    if res > 0 {
+    if res > 0{
         return 1
+    } else if res == 0{
+        return 0
     }
     return -1
 }

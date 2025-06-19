759ec699206711a99fcdc41d58a42fe27e4efc8c
praveshdev3 <122818044+praveshdev3@users.noreply.github.com>
2025-06-19 15:45:24 +0000

    [LC Sync] - Count Equal and Divisible Pairs in an Array - Runtime - 0 ms (100.00%), Memory - 4.4 MB (100.00%)

--- problems/2277-count-equal-and-divisible-pairs-in-an-array/solution.go
+++ problems/2277-count-equal-and-divisible-pairs-in-an-array/solution.go
@@ -2,7 +2,7 @@ func countPairs(nums []int, k int) int {
     count := 0
     for i:=0; i<len(nums); i++{
         for j:=i+1; j<len(nums); j++{
-            if nums[i] == nums[j] && (i*j)%k == 0  {
+            if nums[i] == nums[j] && (i*j)%k == 0{
                 count++
             }
         }

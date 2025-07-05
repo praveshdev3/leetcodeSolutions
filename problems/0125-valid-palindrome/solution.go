bf0dd066abc4ffc70104634ab2a03280b553da78
praveshdev3 <122818044+praveshdev3@users.noreply.github.com>
2025-07-05 12:43:03 +0000

    [LC Sync] - Valid Palindrome - Runtime - 0 ms (100.00%), Memory - 4.8 MB (60.92%)

--- problems/0125-valid-palindrome/solution.go
+++ problems/0125-valid-palindrome/solution.go
@@ -1,21 +1,18 @@
 func isPalindrome(s string) bool {
-    f := func(r rune) rune{
+    lowercase := func(r rune)rune{
         if !unicode.IsLetter(r) && !unicode.IsNumber(r){
             return -1
         }
         return unicode.ToLower(r)
     }
 
-    s = strings.Map(f,s)
+    str := strings.Map(lowercase,s)
 
-    i,j := 0,len(s)-1
-
-    for i<j{
-       if s[i] != s[j]{
+    for i,j := 0,len(str)-1; i<j; i,j = i+1,j-1{
+        if str[i] != str[j]{
             return false
-       } 
-       i++
-       j--
+        }
     }
+
     return true
 }

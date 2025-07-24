0a98484ef93b289841fa7bc506b8ea45e231685a
praveshdev3 <122818044+praveshdev3@users.noreply.github.com>
2025-07-24 16:10:19 +0000

    [LC Sync] - Rotate Array - Runtime - 0 ms (100.00%), Memory - 9.4 MB (79.12%)

--- problems/0189-rotate-array/solution.go
+++ problems/0189-rotate-array/solution.go
@@ -1,12 +1,12 @@
 func rotate(nums []int, k int)  {
     k = k%len(nums)
-    for i,j:=0,len(nums)-1;i<j;i,j=i+1,j-1{
+    for i,j:=0,len(nums)-1; i<=j; i,j=i+1,j-1{
         nums[i],nums[j] = nums[j],nums[i]
     }
-    for i,j:=0,k-1;i<j;i,j=i+1,j-1{
+    for i,j:=0,k-1; i<=j; i,j=i+1,j-1{
         nums[i],nums[j] = nums[j],nums[i]
     }
-    for i,j:=k,len(nums)-1;i<j;i,j=i+1,j-1{
+    for i,j:=k,len(nums)-1; i<=j; i,j=i+1,j-1{
         nums[i],nums[j] = nums[j],nums[i]
     }
 }

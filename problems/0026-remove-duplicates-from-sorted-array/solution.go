b1113f1463e8258649c75d952483f370517e3860
praveshdev3 <122818044+praveshdev3@users.noreply.github.com>
2025-07-24 15:58:13 +0000

    [LC Sync] - Remove Duplicates from Sorted Array - Runtime - 0 ms (100.00%), Memory - 6.3 MB (25.15%)

--- problems/0026-remove-duplicates-from-sorted-array/solution.go
+++ problems/0026-remove-duplicates-from-sorted-array/solution.go
@@ -1,14 +1,10 @@
 func removeDuplicates(nums []int) int {
-     mp := make(map[int]bool)
-    k := 0
-
-    for _,num := range nums {
-        if _,ok := mp[num]; !ok {
-            mp[num] = true
-            nums[k] = num
-            k++
+    index := 1
+    for i:=1; i<len(nums); i++{
+        if nums[i] != nums[i-1]{
+            nums[index] = nums[i]
+            index++
         }
     }
-
-    return k
+    return index
 }

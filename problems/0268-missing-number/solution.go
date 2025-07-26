68574208f7389a764c4fbdf75952f21877b9e5be
praveshdev3 <122818044+praveshdev3@users.noreply.github.com>
2025-07-26 06:29:27 +0000

    [LC Sync] - Missing Number - Runtime - 0 ms (100.00%), Memory - 7.9 MB (87.74%)

--- problems/0268-missing-number/solution.go
+++ problems/0268-missing-number/solution.go
@@ -1,10 +1,8 @@
 func missingNumber(nums []int) int {
     sum,arrSum := 0,0
-
-    for i:=0; i<len(nums); i++{
-        sum = sum+i+1
-        arrSum += nums[i] 
+    for i,num := range nums{
+        sum += i+1
+        arrSum += num
     }
-
     return sum-arrSum
 }

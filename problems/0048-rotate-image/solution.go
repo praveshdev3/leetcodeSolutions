7c9712c6e34360fc722aafc22752cc30c4565827
praveshdev3 <122818044+praveshdev3@users.noreply.github.com>
2025-08-03 11:47:31 +0000

    [LC Sync] - Rotate Image - Runtime - 0 ms (100.00%), Memory - 4.1 MB (63.67%)

---
+++ problems/0048-rotate-image/solution.go
@@ -0,0 +1,11 @@
+func rotate(matrix [][]int)  {
+    for i:=0; i<len(matrix); i++{
+        for j:=i; j<len(matrix[0]); j++{
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        }
+
+        for l,r := 0,len(matrix[0])-1; l<r; l,r=l+1,r-1{
+            matrix[i][l], matrix[i][r] = matrix[i][r], matrix[i][l]
+        }
+    }
+}

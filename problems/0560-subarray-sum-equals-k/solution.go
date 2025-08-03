9228d93d35ca020701dc82c3fd192312874a0f50
praveshdev3 <122818044+praveshdev3@users.noreply.github.com>
2025-08-03 15:55:26 +0000

    [LC Sync] - Subarray Sum Equals K - Runtime - 18 ms (61.75%), Memory - 9.3 MB (62.04%)

--- problems/0560-subarray-sum-equals-k/solution.go
+++ problems/0560-subarray-sum-equals-k/solution.go
@@ -1,8 +1,8 @@
 func subarraySum(nums []int, k int) int {
     prefix := make(map[int]int)
-    prefix[0] = 1
     sum := 0
     count := 0
+    prefix[0]++
 
     for _,num := range nums{
         sum += num

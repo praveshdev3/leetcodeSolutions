70ae74cb8350a99a8c38b52cdcad0b5967877cf7
praveshdev3 <122818044+praveshdev3@users.noreply.github.com>
2025-08-24 03:23:26 +0000

    [LC Sync] - Binary Search - Runtime - 0 ms (100.00%), Memory - 8.8 MB (54.88%)

--- problems/0792-binary-search/solution.go
+++ problems/0792-binary-search/solution.go
@@ -2,13 +2,13 @@ func search(nums []int, target int) int {
     low := 0
     high := len(nums)-1
     for low <= high{
-        mid := (low + high)/2
-        if nums[mid] == target{
-            return mid
-        } else if nums[mid] < target{
-             low = mid+1
-        } else{
+        mid := (low+high)/2
+        if nums[mid] > target{
             high = mid-1
+        }else if nums[mid] < target{
+            low = mid+1
+        }else{
+            return mid
         }
     }
     return -1

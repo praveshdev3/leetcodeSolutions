124b3504d897323527da4e65c429a3cafe58c496
praveshdev3 <122818044+praveshdev3@users.noreply.github.com>
2025-09-04 02:16:14 +0000

    [LC Sync] - Longest Common Prefix - Runtime - 0 ms (100.00%), Memory - 4.4 MB (31.05%)

--- problems/0014-longest-common-prefix/solution.go
+++ problems/0014-longest-common-prefix/solution.go
@@ -1,16 +1,18 @@
 func longestCommonPrefix(strs []string) string {
-   if len(strs) == 0{
-    return ""
-   }
+    if len(strs) <= 0{
+        return ""
+    }
 
     prefix := strs[0]
+
     for i:=1; i<len(strs); i++{
-        for strings.Index(strs[i], prefix) != 0{
+        for strings.Index(strs[i],prefix) != 0{
             prefix = prefix[:len(prefix)-1]
             if prefix == ""{
                 return ""
             }
         }
     }
+
     return prefix
 }

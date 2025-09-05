966b3cdff9ce6d7fd0430cb45d5fafc1777a7c13
praveshdev3 <122818044+praveshdev3@users.noreply.github.com>
2025-09-05 01:59:28 +0000

    [LC Sync] - Rotate String - Runtime - 0 ms (100.00%), Memory - 3.9 MB (91.43%)

---
+++ problems/0812-rotate-string/solution.go
@@ -0,0 +1,9 @@
+func rotateString(s string, goal string) bool {
+    if len(s) != len(goal){
+        return false
+    }
+
+    doubleString := s+s
+
+    return strings.Index(doubleString, goal) >= 0
+}

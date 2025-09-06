290a4d90c67ea0eba4e1eaf808ba045b86983d36
praveshdev3 <122818044+praveshdev3@users.noreply.github.com>
2025-09-06 14:10:48 +0000

    [LC Sync] - Longest Palindromic Substring - Runtime - 3 ms (59.30%), Memory - 7.6 MB (21.28%)

---
+++ problems/0005-longest-palindromic-substring/solution.go
@@ -0,0 +1,37 @@
+func longestPalindrome(s string) string {
+    T := "^#" + strings.Join(strings.Split(s, ""), "#") + "#$"
+    n := len(T)
+    P := make([]int, n)
+    C, R := 0, 0
+
+    for i:=1; i<n-1; i++{
+        if R>i{
+            P[i] = min(R-i, P[2*C - i])
+        }
+        for T[i+1+P[i]] == T[i-1-P[i]]{
+            P[i]++
+        }
+        if i+P[i] > R{
+            R = i+P[i]
+            C = i
+        }
+    }
+
+    maxLen := 0
+    centerIndex := 0
+    for i, v := range P{
+        if v > maxLen{
+            maxLen = v
+            centerIndex = i
+        }
+    }
+
+    return s[(centerIndex-maxLen)/2 : (centerIndex+maxLen)/2]
+}
+
+func min(a,b int)int{
+    if a<b{
+        return a
+    }
+    return b
+}

3b6e232950b49cbb51adb969dfe22fe40b3e7c0d
praveshdev3 <122818044+praveshdev3@users.noreply.github.com>
2025-09-06 04:15:42 +0000

    [LC Sync] - Roman to Integer - Runtime - 0 ms (100.00%), Memory - 4.8 MB (21.35%)

--- problems/0013-roman-to-integer/solution.go
+++ problems/0013-roman-to-integer/solution.go
@@ -1,26 +1,28 @@
 func romanToInt(s string) int {
-    hm := make(map[uint8]int)
-    hm['I'] = 1
-    hm['V'] = 5
-    hm['X'] = 10
-    hm['L'] = 50
-    hm['C'] = 100
-    hm['D'] = 500
-    hm['M'] = 1000
+    mp := make(map[byte]int)
+    mp['M'] = 1000
+    mp['D'] = 500
+    mp['C'] = 100
+    mp['L'] = 50
+    mp['X'] = 10
+    mp['V'] = 5
+    mp['I'] = 1
 
-    number := 0
-    lastNum := 0
-    for i:=len(s)-1; i>=0; i--{
-        x,ok := hm[s[i]]; if !ok{
-            return number
+    num := 0
+    for i:=0; i<len(s); i++{
+        if i<len(s)-1{
+            val, _ := mp[s[i]]
+            val1, _ := mp[s[i+1]]
+            if val >= val1{
+                num += val
+            }else{
+                num -= val
+            }
+        }else{
+            val, _ := mp[s[i]]
+            num += val
         }
-        if x < lastNum{
-            number -= x
-        } else {
-            number += x
-        }
-        lastNum = x
     }
-    return number
-}
 
+    return num
+}

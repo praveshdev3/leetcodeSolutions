9ab5523e8e4bd0039664848f06bea22904ad6478
praveshdev3 <122818044+praveshdev3@users.noreply.github.com>
2025-09-12 02:05:31 +0000

    [LC Sync] - Add Two Numbers - Runtime - 0 ms (100.00%), Memory - 6.2 MB (94.43%)

--- problems/0002-add-two-numbers/solution.go
+++ problems/0002-add-two-numbers/solution.go
@@ -6,28 +6,29 @@
  * }
  */
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-    dummyHead := &ListNode{Val:0}
-    curr := dummyHead
+    if l1 == nil || l2 == nil{
+        return nil
+    }
+
+    result := &ListNode{}
+    res := result
+
     carry := 0
-    for l1 != nil || l2 !=nil || carry != 0 {
-        x := 0
-        if l1 != nil {
-            x = l1.Val
-        }
-        y := 0
-        if l2 != nil {
-            y = l2.Val
-        }
-        sum := carry + x + y
-        carry = sum/10
-        curr.Next = &ListNode{Val: sum % 10}
-        curr = curr.Next
-        if l1 != nil {
+    for l1 != nil || l2 != nil || carry > 0{
+        node := &ListNode{}
+        if l1 != nil{
+            carry += l1.Val
             l1 = l1.Next
         }
-        if l2 != nil {
+        if l2 != nil{
+            carry += l2.Val
             l2 = l2.Next
         }
+        node.Val = carry%10
+        carry = carry/10 
+        result.Next = node
+        result = result.Next
     }
-    return dummyHead.Next
+
+    return res.Next
 }
